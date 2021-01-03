35062510aba07526f2a2bbb126e0f672cc68a755
mjb <mjb@localhost.localdomain>
2021-01-03 11:00:38 +0330

    update (test1): insert dummy data commented

--- test1/index.js
+++ test1/index.js
@@ -4,31 +4,21 @@ const Role = require("./role");
 
 sequelize
   .getSeq()
-  .sync({ force: false })
-  .then(() => {
-    for (let i = 0; i < 5; i++) {
-      Role.create({
-        name: `role${i}`,
-      })
-        .then((role) => console.log(`${role.name} created`))
-        .catch(console.error);
-    }
-    for (let i = 0; i < 10; i++) {
-      User.create({
-        name: `user${i}`,
-      })
-        .then((user) => {
-          let role = Role.findOne({ where: { name: "role0" } });
-          user.getUserHasRole().then(console.log).catch(console.error);
-          user.setUserHasRole(role.id).then(console.log).catch(console.error);
-        })
-        .catch(console.error);
-    }
-  })
-  .catch(console.error);
-
-User.findOne({ where: { name: "user1" } })
-  .then((user) => {
-    // user.getRoles().then(console.log).catch(console.error);
+  .sync({ force: true })
+  .then(async () => {
+    // let role, user;
+    // for (let i = 0; i < 5; i++) {
+    //   role = await Role.create({
+    //     name: `role${i}`,
+    //   });
+    // }
+    // for (let i = 0; i < 10; i++) {
+    //   let user = await User.create({
+    //     name: `user${i}`,
+    //   });
+    //   await user.setUserRole(role.id);
+    // }
+    // user = await User.findOne({ where: { name: "user1" } });
+    // console.log(await user.getRoles());
   })
   .catch(console.error);
