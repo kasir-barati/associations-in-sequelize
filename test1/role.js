bd7430f1f39d647e055565cdea435d8bec7007fb
mjb <mjb@localhost.localdomain>
2021-01-13 09:27:48 +0330

    refactor (test1): Role model refacrored.

--- test1/role.js
+++ test1/role.js
@@ -2,15 +2,26 @@ const { DataTypes, Model } = require("sequelize");
 
 const sequelize = require("../sequelize");
 
-class Role extends Model {}
+class Role extends Model {
+  static col = {
+    id: "id",
+    name: "name",
+    isBasic: "isBasic",
+  };
+  static alias = {
+    users: "RoleHasUsers",
+  };
+}
+
 Role.init(
   {
-    id: {
+    [Role.col.id]: {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4,
     },
-    name: DataTypes.STRING,
+    [Role.col.name]: DataTypes.STRING,
+    [Role.col.isBasic]: DataTypes.BOOLEAN,
   },
   {
     sequelize: sequelize.getSeq(),
