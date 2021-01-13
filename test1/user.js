3c39d4a302a880bcea4a0a425ce8602c13167b92
mjb <mjb@localhost.localdomain>
2021-01-13 09:30:49 +0330

    refactor (test1): User model refactored, use col & alias

--- test1/user.js
+++ test1/user.js
@@ -2,15 +2,29 @@ const { DataTypes, Model } = require("sequelize");
 
 const sequelize = require("../sequelize");
 
-class User extends Model {}
+class User extends Model {
+  static col = {
+    id: "id",
+    email: "email",
+    fullname: "fullname",
+    password: "password",
+    roleId: "roleId",
+  };
+  static alias = {
+    roles: "UserRoles",
+  };
+}
+
 User.init(
   {
-    id: {
+    [User.col.id]: {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4,
     },
-    name: DataTypes.STRING,
+    [User.col.email]: DataTypes.STRING,
+    [User.col.fullname]: DataTypes.STRING,
+    [User.col.password]: DataTypes.STRING,
   },
   {
     sequelize: sequelize.getSeq(),
